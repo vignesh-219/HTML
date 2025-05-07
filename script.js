4bf0d4e3501e01b0ad9e9e9802a2213be97c58ad
Madhubalapandiaraj <madhubala8421@gmail.com>
2025-05-07 21:05:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function showEmail() {
+    document.getElementById("email").style.display = "block";
+    document.getElementById("ph").style.display = "block";
+    document.getElementById("gh").style.display = "block";
+    document.getElementById("lin").style.display = "block";
+}
